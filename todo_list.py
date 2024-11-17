8dd3a35d56c617707b7df2d7b1fa920b7bba28ad
Babalola Samson <babalolasamson0210@gmail.com>
2024-11-17 17:08:01 +0100

    First Commit

---
+++ todo_list.py
@@ -0,0 +1,26 @@
+
+Sunday = ["Meal Prepare", "Plan week ahead", "Leisure time"]
+Monday = ["Code review", "report submission", "Yoga"]
+Tuesday = ["Client call", "Marketing strategy", "Hangout"]
+Wednesday = ["Team meeting", "Project planning", "Grocery Shopping"]
+Thursday = ["Project development","Team lunch", "Pay bills"]
+Friday = ["Project deadline", "Review week progress", "Weekend Planning"]
+Saturday = ["Outdoor activities", "Errands", "Relaxation time"]
+
+print(Sunday[0])
+print(Sunday[1])
+print(Sunday[2])
+for item in Sunday:
+    print(item)
+for item in Monday:
+    print(item)
+for item in Tuesday:
+    print(item)
+for item in Wednesday:
+    print(item)
+for item in Thursday:
+    print(item)
+for item in Friday:
+    print(item)
+for item in Saturday:
+    print(item)
